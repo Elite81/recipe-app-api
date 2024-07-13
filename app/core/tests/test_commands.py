54f3a235135a1b4eb6af3a0f375c872ce0e26a49
Elite81 <scophield@outlook.com>
2024-07-13 07:44:30 +0000

    Configuerd docker compose and checks to wait for db 2

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -24,8 +24,10 @@ class CommandTests(SimpleTestCase):
     @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database get operation error"""
-        patched_check.side_effect = [Pscop2gError] * 2 + [OperationalError] * 3 + [True]
-
+        patched_check.side_effect = [Pscop2gError] * 2
+        patched_check.side_effect += +[OperationalError] * 3
+        patched_check.side_effect += [True]
+        
         call_command("wait_for_db")
 
         self.assertEqual(patched_check.call_count, 6)
